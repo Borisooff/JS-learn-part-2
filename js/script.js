e320680291b2e3363a7fb0e21d391f2491dfe374
Daniil <danechka.bo@yandex.ru>
2023-03-13 15:37:44 +0300

    events

--- js/script.js
+++ js/script.js
@@ -1,38 +1,99 @@
 'use strict';
-const 
-    promoBg = document.querySelector('.promo__bg'),
-    promoInteractiveList = document.querySelector('.promo__interactive-list'),
-    gener = promoBg.querySelector('.promo__genre');
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против...",
-        "Лига справедливости",
-    ]
-};
+document.addEventListener('DOMContentLoaded', () => {
+    const promoBg = document.querySelector('.promo__bg'),
+        promoInteractiveList = document.querySelector('.promo__interactive-list'),
+        gener = promoBg.querySelector('.promo__genre'),
+        subBtn = document.querySelector('.add__btn'),
+        addInput = document.querySelector('.adding__input'),
+        favoreatCheck = document.querySelector('.favoreat'),
+        adv = document.querySelectorAll('.promo__adv img');
 
-// удаление рекламных блоков
-document.querySelectorAll('.promo__adv img').forEach(item => {
-    item.remove();
-});
 
-//замена жанра на промопостере
-gener.textContent = 'Драмма';
+    const movieDB = {
+        movies: [
+            // "Логан",
+            // "Ла-ла лэнд",
+            // "Одержимость",
+            // "Скотт Пилигрим против...",
+            // "Лига справедливости",
+        ]
+    };
 
-// замена промопостера
-promoBg.style.cssText = 'background: url("img/bg.jpg") center center/cover no-repeat;';
+    // удаление рекламных блоков
+    const deleteAdv = (arr) => {
+        arr.forEach(item => {
+            item.remove();
+        });
+    }
+    deleteAdv(adv);
 
-// сортировка массива с фильмами и добавление их на страницу
-movieDB.movies.sort();
-for (let i = 0; i < movieDB.movies.length; i++) {
-    promoInteractiveList.insertAdjacentHTML('beforeend', `
-    <li class="promo__interactive-item"> ${i + 1} ${movieDB.movies[i]}
-        <div class="delete"></div>
-    </li>`)
-};
+    const makeChange = () => {
+    //замена жанра на промопостере
+    gener.textContent = 'Драмма';
+    // замена промопостера
+    promoBg.style.cssText = 'background: url("img/bg.jpg") center center/cover no-repeat;';
+    }
+    makeChange();
+
+    // по нажатию кнопки отправки срабатывает функция добавления объекта в массив с фильмами.
+    // если нажать при пустой строке появится красная тень
+    // если фильм длинее 21 символа, строка обрезается и добавляется "...".Затем отправляется в массив. 
+    subBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (addInput.value != '') {
+            if (addInput.value.length > 21) {
+                addInput.value = addInput.value.slice(0, 21) + '...';
+            }
+            pushFilm();
+        } else {
+            addInput.style.boxShadow = '0px 0px 4px 3px rgba(255, 0, 0, 0.2)';
+        }
+    })
+
+
+    // сортировка массива с фильмами и добавление их на страницу
+    // перед показом фильмов на страницу список очищается, затем добавляется новый на страницу
+    // после добавления на страницу получаем кнопку delete по нажатию на которую из массива с фильмами вырезается фильм под индеком кнопки и функция показа фильмов срабатывает снова 
+    function showFilms() {
+        movieDB.movies.sort();
+        promoInteractiveList.innerHTML = '';
+        for (let i = 0; i < movieDB.movies.length; i++) {
+            promoInteractiveList.insertAdjacentHTML('beforeend', `
+        <li class="promo__interactive-item"> ${i + 1} ${movieDB.movies[i]}
+            <div class="delete"></div>
+        </li>`)
+        }
+        const deleteBtns = promoInteractiveList.querySelectorAll('.delete');
+        for (let i = 0; i < deleteBtns.length; i++) {
+            deleteBtns[i].addEventListener('click', () => {
+                movieDB.movies.splice(i, 1);
+                showFilms();
+            })
+        }
+    }
+
+
+    //  добавляет знаение введенное пользователем в массив с фильмами и показывает фильмы на страницу с помощью showFilm
+    // убирает красную тень когда фильмы добавленны
+    //  если стоит check добавить в избранное воводится log об этом. check убирается после каждого добавления
+    function pushFilm() {
+        movieDB.movies.push(addInput.value);
+        showFilms();
+        addInput.style.boxShadow = '0px 0px 0px 0px';
+        if (favoreatCheck.checked) {
+            console.log(`${addInput.value} добавлен в любимые фильмы`);
+            favoreatCheck.checked = false;
+        }
+        addInput.value = '';
+    }
+
+    
+})
+
+
+
+
 
 
 
